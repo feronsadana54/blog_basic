e5ea938faabdd60a14eb89061a569e7accbfee90
Feron Sadana <feronsadana16@gmail.com>
2022-05-08 19:41:31 +0700

    create function new to Post

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,6 +1,7 @@
 class PostController < ApplicationController
 
     def index
+        @post = Post.all
     end
 
     def new
@@ -8,9 +9,14 @@ class PostController < ApplicationController
 
     def create 
         @post = Post.new(post_params)
+        if @post.save
+            redirect_to root_path
+        else 
+            render :plain => @post.errors.inspect
+        end
     end
 
     private def post_params 
-        params.require(:post).inspect
+        params.require(:post).permit(:title, :body)
     end
 end
