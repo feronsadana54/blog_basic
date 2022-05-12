18c05a9fdc3f9f42e6d2d96d9509094d30d439de
Feron Sadana <feronsadana16@gmail.com>
2022-05-12 19:00:28 +0700

    add validate image

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
 class Post < ApplicationRecord
     has_one_attached :image
+    validates :title, presence: true
+    validates :body, presence: true 
+    validate :validate_image 
+
+    private def validate_image 
+        if image.attached? && !image.content_type.in?(%w[image/jpeg image/png])
+            errors.add(:image, 'must be a JPEG or PNG')
+        elsif image.attached? == false 
+            errors.add(:image, 'must be attached')
+        end
+    end
 end
